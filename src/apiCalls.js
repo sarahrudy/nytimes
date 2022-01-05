27b4bd0999fb1ebefc24c7174ad3df3020048637
Sarah <sarahrudy@gmail.com>
2022-01-04 21:55:36 -0700

    Fetch articles from API

---
+++ src/apiCalls.js
@@ -0,0 +1,5 @@
+export const getData = (category) => {
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=oElLadX78g1ROpCXcF5DRym7bsJAlQoM`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,31 @@
+import { useEffect, useState } from 'react'
+import { getData } from '../../apiCalls'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [articles, setArticles ] = useState([])
+
+  useEffect(() => {
+    getData('arts')
+    .then(data => setArticles(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://www.nytimes.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NY Times
-        </a>
+        <p>The New York Times</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+      // <a
+      //     className="App-link"
+      //     href="https://www.nytimes.com/"
+      //     target="_blank"
+      //     rel="noopener noreferrer"
+      //   >
+      //     NY Times
+      //   </a>
