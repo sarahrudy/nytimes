c282c05bf89075d1830c221bb1e3dc38230d752d
Sarah <sarahrudy@gmail.com>
2022-01-05 00:38:32 -0700

    Render articles on page after fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,29 @@
 import { useEffect, useState } from 'react'
 import { getData } from '../../apiCalls'
 import './App.css';
+import Header from '../Header/Header'
+import Articles from '../Articles/Articles'
+import ArticleDetails from '../ArticleDetails/ArticleDetails'
+import Footer from '../Footer/Footer'
 
 const App = () => {
   const [articles, setArticles ] = useState([])
+  const [error, setError ] = useState(null)
 
   useEffect(() => {
-    getData('arts')
-    .then(data => setArticles(data))
+    getData('home')
+    .then(data => setArticles(data.results))
+    .catch(error => setError(error))
   }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>The New York Times</p>
-      </header>
+      <Header />
+      <Articles articles={ articles } />
+      <ArticleDetails />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-      // <a
-      //     className="App-link"
-      //     href="https://www.nytimes.com/"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   >
-      //     NY Times
-      //   </a>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,11 @@
 import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="copyright">©️ The New York Times 2022</p>
+    </div>
+  )
+}
+
+export default Footer 
