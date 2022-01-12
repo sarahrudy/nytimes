f4e2c9f107e765b8f2dda8a13476ed1ce7483795
Sarah <sarahrudy@gmail.com>
2022-01-11 18:30:58 -0700

    Update fetch call to fetch articles by category

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { getData } from '../../apiCalls'
 import './App.css';
 import Header from '../Header/Header'
+import Categories from '../Categories/Categories'
 import Articles from '../Articles/Articles'
 import Footer from '../Footer/Footer'
 
@@ -9,15 +10,20 @@ const App = () => {
   const [articles, setArticles ] = useState([])
   const [error, setError ] = useState(null)
 
-  useEffect(() => {
-    getData('home')
+  const setCategory = (category) => {
+    getData(category)
     .then(data => setArticles(data.results))
     .catch(error => setError(error))
+  }
+
+  useEffect(() => {
+    setCategory('world')
   }, [])
 
   return (
     <div className="App">
       <Header />
+      <Categories setCategory={ setCategory } />
       <Articles articles={ articles } />
       <Footer />
     </div>
