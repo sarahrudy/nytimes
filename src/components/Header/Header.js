6368a7e93e2e0ae2e9d548a11d333ad673b50b15
Sarah <sarahrudy@gmail.com>
2022-01-11 19:48:18 -0700

    Add date to header and style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import './Header.css'
 import NYTlogo from '../../images/the-new-york-times.png'
+import dayjs from 'dayjs'
 
 const Header = () => {
+
+  const today = dayjs(new Date())
+
   return (
-    <div>
+    <div className="header-container">
       <img src={ NYTlogo } className="nyt-logo"></img>
+      <h1 className="date">{ today.format('dddd, MMMM DD, YYYY') }</h1>
     </div>
   )
 }
